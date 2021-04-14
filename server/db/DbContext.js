5d83ce40391f1ebdffeb0203bfcbdfc6cadcb028
Thomas Swisher <thomas@structuredseo.com>
2021-04-14 13:02:10 -0600

    init

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import CarSchema from "../models/Car";
 import ValueSchema from "../models/Value";
+import HouseSchema from "../models/House";
 
 
 // this is where models get registered and access from the db
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Cars = mongoose.model("Car", CarSchema);
+  Houses = mongoose.model("House", HouseSchema);
 }
 
 export const dbContext = new DbContext();
